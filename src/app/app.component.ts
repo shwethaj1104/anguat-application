09eac37e628ba6b4658d244dbf482b10af0cbfbb
shwethaj1104 <shwethaj1104@gmail.com>
2022-11-05 13:02:00 +0530

    Completed Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getData } from './data';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-application';
+export class AppComponent implements OnInit{
+  rowData: any = [];
+  public defaultColDef: any;
+  public columnDefs: any;
+  constructor(){}
+  ngOnInit(){
+    this.columnDefs = [
+      { headerName: 'SI No', field: 'orderId', flex:1},
+      { headerName: 'Ordered By', field: 'orderedBy'},
+      { headerName: 'Account Type', field: 'account'},
+      { headerName: 'Order Date', field: 'orderDate' },
+      { headerName: 'Order Total', field: 'orderTotal' },
+    ]
+    this.defaultColDef = {
+      sortable: true,
+      minWidth: 100,
+      resizable: true,
+    };
+    console.log(getData)
+    this.rowData=getData
+  }
 }
---
+++ src/app/data.ts
@@ -0,0 +1,163 @@
+
+export const getData = [
+    {
+      orderId: "1",
+      custonerPO: "customer",
+      account: "Customer",
+      orderDate: "01-28-2022",
+      orderedBy: "Rushi",
+      orderTotal: "$ 567.98"
+    },
+    {
+      orderId: "2",
+      custonerPO: "acustomer",
+      account: "Admin",
+      orderDate: "02-28-2022",
+      orderedBy: "Rushi",
+      orderTotal: "$ 467.98"
+    },
+    {
+      orderId: "3",
+      custonerPO: "customer",
+      account: "Manager",
+      orderDate: "03-28-2022",
+      orderedBy: "Rushi",
+      orderTotal: "$ 597.98"
+    },
+    {
+      orderId: "4",
+      custonerPO: "customer",
+      account: "Admin",
+      orderDate: "04-28-2022",
+      orderedBy: "Rushi",
+      orderTotal: "$ 577.98"
+    },
+    {
+      orderId: "5",
+      custonerPO: "customer",
+      account: "Customer",
+      orderDate: "01-28-2022",
+      orderedBy: "Rushi",
+      orderTotal: "$ 257.98"
+    },
+    {
+        orderId: "6",
+        custonerPO: "customer",
+        account: "Customer",
+        orderDate: "01-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 567.98"
+      },
+      {
+        orderId: "7",
+        custonerPO: "acustomer",
+        account: "Admin",
+        orderDate: "02-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 467.98"
+      },
+      {
+        orderId: "8",
+        custonerPO: "customer",
+        account: "Manager",
+        orderDate: "03-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 597.98"
+      },
+      {
+        orderId: "9",
+        custonerPO: "customer",
+        account: "Admin",
+        orderDate: "04-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 577.98"
+      },
+      {
+        orderId: "10",
+        custonerPO: "customer",
+        account: "Customer",
+        orderDate: "01-28-2022",
+        orderedBy: "Murali",
+        orderTotal: "$ 257.98"
+      },
+      {
+        orderId: "11",
+        custonerPO: "customer",
+        account: "Service Center",
+        orderDate: "01-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 567.98"
+      },
+      {
+        orderId: "12",
+        custonerPO: "acustomer",
+        account: "Admin",
+        orderDate: "02-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 467.98"
+      },
+      {
+        orderId: "13",
+        custonerPO: "customer",
+        account: "Manager",
+        orderDate: "03-28-2022",
+        orderedBy: "Murali",
+        orderTotal: "$ 597.98"
+      },
+      {
+        orderId: "14",
+        custonerPO: "customer",
+        account: "Admin",
+        orderDate: "04-28-2022",
+        orderedBy: "Rashi",
+        orderTotal: "$ 577.98"
+      },
+      {
+        orderId: "15",
+        custonerPO: "customer",
+        account: "VIP Customer",
+        orderDate: "01-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 257.98"
+      },
+      {
+        orderId: "16",
+        custonerPO: "customer",
+        account: "Customer",
+        orderDate: "01-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 567.98"
+      },
+      {
+        orderId: "17",
+        custonerPO: "acustomer",
+        account: "Admin",
+        orderDate: "02-28-2022",
+        orderedBy: "Rashi",
+        orderTotal: "$ 467.98"
+      },
+      {
+        orderId: "18",
+        custonerPO: "customer",
+        account: "Manager",
+        orderDate: "03-28-2022",
+        orderedBy: "Anjul",
+        orderTotal: "$ 597.98"
+      },
+      {
+        orderId: "19",
+        custonerPO: "customer",
+        account: "Admin",
+        orderDate: "04-28-2022",
+        orderedBy: "Rushi",
+        orderTotal: "$ 577.98"
+      },
+      {
+        orderId: "20",
+        custonerPO: "customer",
+        account: "VIP",
+        orderDate: "01-28-2022",
+        orderedBy: "Anjul",
+        orderTotal: "$ 257.98"
+      },
+  ]
