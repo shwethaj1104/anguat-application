f900901a15b87af706fbeca223820139674d409a
shwethaj1104 <shwethaj1104@gmail.com>
2022-11-07 13:16:00 +0530

    Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ import 'ag-grid-community/styles/ag-theme-alpine.css';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   rowData: any = [];
   public defaultColDef: any;
   public columnDefs: any;
-  showSideBar: boolean=false;
-  constructor(){}
-  ngOnInit(){
+  showSideBar: boolean = false;
+  constructor() { }
+  ngOnInit() {
 
     this.columnDefs = [
-      { headerName: 'SI No', field: 'orderId'},
-      { headerName: 'Ordered By', field: 'orderedBy'},
-      { headerName: 'Account Type', field: 'account'},
+      { headerName: 'SI No', field: 'orderId' },
+      { headerName: 'Ordered By', field: 'orderedBy' },
+      { headerName: 'Account Type', field: 'account' },
       { headerName: 'Order Date', field: 'orderDate' },
       { headerName: 'Order Total', field: 'orderTotal' },
     ]
@@ -27,20 +27,20 @@ export class AppComponent implements OnInit{
     this.defaultColDef = {
       sortable: true,
       resizable: true,
-      flex:1,
-      filter:true
+      flex: 1,
+      filter: true
     };
 
-    this.rowData=getData
+    this.rowData = getData
 
   }
-  showSidebar(){
-    this.showSideBar=true
+  showSidebar() {
+    this.showSideBar = true
     const ele = document.querySelector<HTMLElement>(".container-header")!;
-    if(ele.style.display==="none"){
-      ele.style.display="block"
-    }else{
-      ele.style.display="none"
+    if (ele.style.display === "none") {
+      ele.style.display = "block"
+    } else {
+      ele.style.display = "none"
     }
   }
 }
